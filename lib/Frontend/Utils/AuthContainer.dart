4f621e7f8590603085b2ffb2d7f76861ff5b2c5f
Haseeb Ahmad <haseebahmad0160@gmail.com>
2026-04-26 14:25:01 +0500

    Initilazing the Frontend structure

---
+++ lib/Frontend/Utils/AuthContainer.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Authcontainer extends StatefulWidget {
+  const Authcontainer({super.key});
+
+  @override
+  State<Authcontainer> createState() => _AuthcontainerState();
+}
+
+class _AuthcontainerState extends State<Authcontainer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
